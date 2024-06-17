9a9072f1acdf754e8c954ef869cbac24cb4f32f8
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-06-17 14:48:06 +0100

    part 28 techniques for building tailwind projects

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body class="bg-black text-white">
